19b112174a91c6c3dfcf831fad757c873ac622d8
camilo ortiz <camilodortiz@gmail.com>
2020-04-22 21:13:14 -0400

    more restrictions

--- solution-10.pl
+++ solution-10.pl
@@ -88,16 +88,13 @@ question10(Invitees, N, Day) :-
 setup(Invitees, N) :-
   % Number of people to invite
   N :: 1..12,
-
-  % How can I create the variables in Invitees dynamically?
-  Invitees = [X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, X11, X12],
+  % Restrict number of people invited to N
+  length(Invitees, N),
   % Invitees are the people we know
   Invitees :: [tom, fred, billy, tim, frank, barry, sue, jane, betty, ellen, joan, betsy],
   % Can't invite the same person more than twice
   alldifferent(Invitees),
 
-  % Restrict number of people invited to N
-  length(Invitees, N),
   % Everyone must be available on the same day
   Day :: [friday, saturday, sunday],
   foreach(member(Person, Invitees), available(Person, Day)),
@@ -114,27 +111,18 @@ setup(Invitees, N) :-
 
   % Number of men and women is the same
 
+
   % Everyone knows someone else
   foreach(member(Person1, Invitees), member(Person2, Invitees), know(Person1, Person2)),
-  % No one dislikes anyone else
 
+  % No one dislikes anyone else
+  foreach(member(Person1, Invitees), member(Person2, Invitees), not(dislike(Person1, Person2))),
   % No mixing republicans and democrats
-  .
+  PoliticalLeaning :: [democrat, republican],
+  foreach(member(Person, Invitees), member(PoliticalLeaning, Person)).
 
 solve(Invitees) :-
   labeling(Invitees).
 
 printI(Invitees) :-
-  Invitees = [X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, X11, X12],
-  printf("X1 is %3d", [X1]),
-  printf("X2 is %3d", [X2]),
-  printf("X3 is %3d", [X3]),
-  printf("X4 is %3d", [X4]),
-  printf("X5 is %3d", [X5]),
-  printf("X6 is %3d", [X6]),
-  printf("X7 is %3d", [X7]),
-  printf("X8 is %3d", [X8]),
-  printf("X9 is %3d", [X9]),
-  printf("X10 is %3d", [X10]),
-  printf("X11 is %3d", [X11]),
-  printf("X12 is %3d", [X12]).
+  foreach(member(Invitee, Invitees), printf("%3d is invited", [Invitee]))..
